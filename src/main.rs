f89bf6ed83df773e23e1537fe0b461b9d0d6c1b0
FBIGlowie <144389458+FBIGlowie@users.noreply.github.com>
2023-11-22 20:52:57 -0500

    Im done good night

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
+use std::time::{Duration, Instant};
+
 fn main() {
+    const TOLLERANCE: f64 = 1e-323;
+    println!("Tollerance set to {}", TOLLERANCE.);
     // following some stack exchange answer on the webz I need to setup sum variables
-    const NUMBER: i128 = 401; //sqrt is 20
-    let mut guess: f64 = 0.0;
+    const NUMBER: i128 = 22222222222222222222222222; 
+    let mut guess: f64 = 1.0;
+    let mut count: i128 = 0;
+    let start = Instant::now();
     loop {
-        if guess * guess >= NUMBER.abs() as f64 {
-            println!("{},{}", guess, guess * guess);
+        count += 1;
+        let root = 0.5 * (guess + NUMBER as f64 / guess);
+        if (root - guess).abs() < TOLLERANCE {
+            println!("Square root: {}", root);
+            println!("Root^2 = {}", root * root);
             break;
-        } else {
-            guess += 0.000000001;
         }
+        guess = root;
     }
+
+    let duration = start.elapsed();
+    println!("In seconds: {}", duration.as_secs());
+    println!("In milliseconds: {}", duration.as_millis());
+    println!("In microseconds: {}", duration.as_micros());
+    println!("In nanoseconds: {}", duration.as_nanos());
 }
-// this garbage is pretty self explanatory
