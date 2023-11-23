68465a5874feacdc4959928fb2795b407dd5090e
FBIGlowie <144389458+FBIGlowie@users.noreply.github.com>
2023-11-22 19:35:57 -0500

    Testing number 1 its still not newton's

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    // following some stack exchange answer on the webz I need to setup sum variables
+    const NUMBER: i128 = 401; //sqrt is 20
+    let mut guess: f64 = 0.0;
+    loop {
+        if guess * guess >= NUMBER.abs() as f64 {
+            println!("{},{}", guess, guess * guess);
+            break;
+        } else {
+            guess += 0.000000001;
+        }
+    }
 }
+// this garbage is pretty self explanatory
